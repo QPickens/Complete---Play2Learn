7cbc63cf29c9e2c37a3e2ee77db971e475a12b7c
Quanesha <quanesha_pickens@yahoo.com>
2024-10-25 14:16:51 -0500

    Initial commit

--- games/views.py
+++ games/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
 class MathFactsView(TemplateView):
     template_name = "math-facts.html"
 
--- play2learn/urls.py
+++ play2learn/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from games.views import HomePageView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', HomePageView.as_view()),
     path('', include("games.urls"))
 ]
