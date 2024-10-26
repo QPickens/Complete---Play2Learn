28c87e947772d381c02ac66ccd1174bf7d634cdc
Quanesha <quanesha_pickens@yahoo.com>
2024-10-25 20:17:13 -0500

    Initial commit

--- games/urls.py
+++ games/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from games.views import MathFactsView, AnagramHuntView
+from games.views import AboutUsView, HomePageView, MathFactsView, AnagramHuntView
 
 app_name = 'games'
 urlpatterns = [
+    path('about-us/', AboutUsView.as_view(), name='about-us'),
+    path('', HomePageView.as_view(), name='homepage'),
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
 ]
--- games/views.py
+++ games/views.py
@@ -6,6 +6,9 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+class AboutUsView(TemplateView):
+    template_name = 'pages/about_us.html'
+
 class MathFactsView(TemplateView):
     template_name = "math-facts.html"
 
